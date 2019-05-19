0d7e8058b3a862d8d958bd061bacb90e4789b63a
Vikas <42447141+babuhacker@users.noreply.github.com>
2019-05-19 03:33:44 -0500

    retrieve data
    retrieve data from json file

---
+++ animal/sketch.js
@@ -0,0 +1,20 @@
+var data;
+
+function preload(){
+	data = loadJSON("bird.json");
+}
+function setup() {
+
+  noCanvas();
+
+  var birds = data.birds;
+  for(var i=0;i<birds.length;i++){
+	  
+	  createElement('h1',birds[i].family);
+	  var members = birds[i].members;
+	  for(var j=0; j<members.length;j++){
+		  createDiv(members[j]);
+	  }
+  }
+	
+}
